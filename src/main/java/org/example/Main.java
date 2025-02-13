eba5fe152b690b94a6dfe46e4d723939d18b2a9c
Simon Thomsson <simtho0624@gmail.com>
2025-02-13 10:35:37 +0100

    Add a basic gui

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Windows.MainWindow;
+
+public class Main {
+    static MainWindow mainWindow = new MainWindow();
+
+    public static void main(String[] args) {
+        mainWindow.RunWindow();
+    }
+}
---
+++ src/main/java/org/example/Windows/MainWindow.java
@@ -0,0 +1,90 @@
+package org.example.Windows;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MainWindow {
+
+    private static JFrame frame = new JFrame();
+    private JPanel topPanel = new JPanel();
+    private JPanel nextTopPanel = new JPanel();
+    private JPanel middlePanel = new JPanel();
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private String username;
+    private String password;
+    private JButton saveButton = new JButton("Save");
+    GridLayout gridLayout = new GridLayout(5,1);
+
+    List<Object> account;
+
+    public void RunWindow(){
+
+        frame.setSize(800,800);
+        frame.setLayout(gridLayout);
+
+        usernameLabel = new JLabel("Username: ");
+        JTextField usernameInput = new JTextField(20);
+
+        passwordLabel = new JLabel("Password: ");
+        JTextField passwordInput = new JTextField(20);
+
+
+        ContentInTheWindow(usernameInput, passwordInput);
+
+
+
+        ButtonAction(usernameInput, passwordInput);
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+    }
+
+    private void ContentInTheWindow(JTextField usernameInput, JTextField passwordInput) {
+        topPanel.add(usernameLabel);
+        topPanel.add(usernameInput);
+
+        nextTopPanel.add(passwordLabel);
+        nextTopPanel.add(passwordInput);
+
+        middlePanel.add(saveButton);
+
+        frame.add(topPanel);
+        frame.add(nextTopPanel);
+        frame.add(middlePanel);
+    }
+
+    private void ButtonAction(JTextField usernameInput, JTextField passwordInput) {
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                username = usernameInput.getText();
+                password = passwordInput.getText();
+
+                if (!Objects.equals(username, "") && !Objects.equals(password, "")){
+                    //System.out.println(username);
+                    //System.out.println(password);
+
+                    account = new ArrayList<>();
+
+                    account.add(username);
+                    account.add(password);
+
+
+                }else {
+                    System.out.println("The text field is empty");
+                }
+            }
+        });
+    }
+}
