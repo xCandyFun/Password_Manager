e9c3c3688ff6c91be54540a5ec692fe1ced296c2
Simon Thomsson <simtho0624@gmail.com>
2025-03-13 11:26:18 +0100

    Fix the input data window

--- src/main/java/org/example/Windows/NewAccountWindow.java
+++ src/main/java/org/example/Windows/NewAccountWindow.java
@@ -1,46 +1,78 @@
 package org.example.Windows;
 
-import org.example.UsbConfig.UsbMonitor;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NewAccountWindow {
 
     private MainWindow mainWindow;
 
+    JFrame frame;
+
+    private final int rows = 16, cols = 4;
+
+    private final int width = 800, height = 800;
+
+    GridLayout gridLayout;
+    JPanel testPanel;
+    JButton button;
+    JTextField textField;
+    JLabel TESTOnPlaceInCell;
+    JLabel accountToWhereLabel;
+    JLabel emailInputLabel;
+    JLabel usernameInputLabel;
+    JLabel passwordInputLabel;
+    JLabel passwordSecondLabel;
+
+    JTextField accountToWhereInput;
+    JTextField emailInput;
+    JTextField usernameInput;
+    JPasswordField passwordInput;
+    JPasswordField passwordSecondInput;
+
+    JButton sendButton;
+
+    List<String> accountInfo = new ArrayList<>();
+
+
     public NewAccountWindow(MainWindow mainWindow){
         this.mainWindow = mainWindow;
+
+        gridLayout = new GridLayout(rows, cols);
+        frame = new JFrame();
+        testPanel = new JPanel(gridLayout);
+        button = new JButton("GO BACK");
+        textField = new JTextField();
+        TESTOnPlaceInCell = new JLabel();
+        accountToWhereLabel = new JLabel("Place: ");
+        emailInputLabel = new JLabel("Email: ");
+        usernameInputLabel = new JLabel("Optional Username: ");
+        passwordInputLabel = new JLabel("Password: ");
+        passwordSecondLabel = new JLabel("Type password Again: ");
+
+
+        accountToWhereInput = new JTextField();
+        emailInput = new JTextField();
+        usernameInput = new JTextField();
+        passwordInput = new JPasswordField();
+        passwordSecondInput = new JPasswordField();
+
+        sendButton = new JButton("SAVE");
+
     }
-
-    JFrame frame = new JFrame();
-
-    int rows = 16;
-    int cols = 4;
-
-    GridLayout gridLayout = new GridLayout(rows, cols);
-
-    JPanel testPanel = new JPanel(gridLayout);
-
-    JButton button = new JButton("GO BACK");
-
-    JTextField textField = new JTextField();
-
-    JLabel TEST = new JLabel();
-
-    int width = 800, height = 800;
 
     public void run(){
 
-        button = new JButton("TEST");
-
         placeholderLayout();
 
-        //testPanel.add(button);
-
         actionButtonGoBack();
+        actionButtonSend();
 
         frame.add(testPanel);
 
@@ -51,6 +83,7 @@ public class NewAccountWindow {
     }
 
     private void placeholderLayout() {
+        testPanel.removeAll();
         int totalCells = rows * cols;
         Component[] components = new Component[totalCells];
 
@@ -60,13 +93,53 @@ public class NewAccountWindow {
 
         // [Location on components]
         // [4 * 4 + 1 is the 9 cell]
-        components[4 * 4 + 1] = textField;
-        components[2 * 2 + 1] = TEST;
+
+        components[3 * 3] = accountToWhereLabel;
+        components[3 * 3 + 1] = accountToWhereInput;
+
+        components[6 * 2 + 1] = emailInputLabel;
+        components[6 * 2 + 2] = emailInput;
+
+        components[8 * 2 + 1] = usernameInputLabel;
+        components[8 * 2 + 2] = usernameInput;
+
+        components[10 * 2 + 1] = passwordInputLabel;
+        components[10 * 2 + 2] = passwordInput;
+
+        components[12 * 2 + 1] = passwordSecondLabel;
+        components[12 * 2 + 2] = passwordSecondInput;
+
+        components[16 * 2 +1 ] = sendButton;
+
         components[totalCells - 1] = button;
 
         for (Component comp : components) {
             testPanel.add(comp);
         }
+
+        sendButton.setEnabled(false);
+
+        ActionListener checkPassword = e -> {
+
+            char[] passwordData = passwordInput.getPassword();
+            char[] passwordSecondData = passwordSecondInput.getPassword();
+
+            boolean bothEmpty = passwordData.length == 0 && passwordSecondData.length == 0;
+            boolean passwordMatch = Arrays.equals(passwordData, passwordSecondData) && !bothEmpty;
+
+            sendButton.setEnabled(passwordMatch);
+
+            Arrays.fill(passwordData,' ');
+            Arrays.fill(passwordSecondData,' ');
+        };
+
+        passwordInput.addActionListener(checkPassword);
+        passwordSecondInput.addActionListener(checkPassword);
+
+        //ActionListener triggers on pressing Enter, but CaretListener detects every keystroke.
+        passwordInput.addCaretListener(e -> checkPassword.actionPerformed(null));
+        passwordSecondInput.addCaretListener( e -> checkPassword.actionPerformed(null));
+
     }
 
     public void actionButtonGoBack(){
@@ -75,10 +148,39 @@ public class NewAccountWindow {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //frame.dispose();
-                frame.setVisible(false);
                 mainWindow.frame.setVisible(true);
+                mainWindow.frame.toFront();
 
-                mainWindow.runMainWindow();
+                mainWindow.frame.validate();
+                mainWindow.frame.repaint();
+
+            }
+        });
+    }
+
+    public void actionButtonSend() {
+
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String accountToWhereData = accountToWhereInput.getText();
+                String emailData = emailInput.getText();
+                String usernameData = usernameInput.getText();
+
+                char[] passwordData = passwordInput.getPassword();
+                char getPasswordData = passwordData[0];
+
+                String passwordDataString = Character.toString(getPasswordData);
+
+                accountInfo.add(accountToWhereData);
+                accountInfo.add(emailData);
+                accountInfo.add(usernameData);
+                accountInfo.add(passwordDataString);
+
+                //TODO call encrypt class to encrypt the data in the List
+                //call the class that take care of the database
+
             }
         });
     }
